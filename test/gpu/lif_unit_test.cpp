9c595c2892cfc4325a93091242ec5085fdcb1e7d
dkb <dennis@bautembach.de>
2020-12-16 16:17:11 +0200

    WIP (sparse connect.)

--- test/gpu/lif_unit_test.cpp
+++ test/gpu/lif_unit_test.cpp
@@ -5,72 +5,30 @@
 
 #include "../../include/BSim.h"
 
+#include <random>
+
 using namespace std;
 
 int main(int argc, char **argv)
 {
-	bool load = false;
-
-	if ((argc > 1) && (0==strcmp(argv[1], "load"))) {
-		load = true;
-	}
-
-	const int N = 500;
+	const int N = 100;
 	Network c;
-	//createPopulation(int id, int N, LIFENeuron(ID id, real v_init, real v_rest, real v_reset, real cm, real tau_m, real tau_refrac, real tau_syn_E, real tau_syn_I, real v_thresh, real i_offset)), ID(0, 0), real tau_syn_E, real tau_syn_I);
-	Population<LIF_brian> *pn0 = c.createPopulation(0, N, LIF_brian(LIFENeuron(0.0, 0.0, 0.0, 1.0e-1, 50.0e-3, 0.0002, 1.0, 1.0, 15.0e-3, 10.0e-1), 1.0, 1.0));
-	Population<LIF_brian> *pn1 = c.createPopulation(1, N, LIF_brian(LIFENeuron(0.0, 0.0, 0.0, 1.0e-1, 50.0e-3, 0.0001, 1.0, 1.0, 15.0e-3, 0.0e-3), 1.0, 1.0));
-	Population<LIF_brian> *pn2 = c.createPopulation(2, N, LIF_brian(LIFENeuron(0.0, 0.0, 0.0, 1.0e-1, 50.0e-3, 0.0, 1.0, 1.0, 15.0e-3, 0.0e-3), 1.0, 1.0));
-	Population<LIF_brian> *pn3 = c.createPopulation(3, N, LIF_brian(LIFENeuron(0.0, 0.0, 0.0, 1.0e-1, 50.0e-3, 0.0, 1.0, 1.0, 15.0e-3, 0.0e-3), 1.0, 1.0));
-	Population<LIF_brian> *pn4 = c.createPopulation(4, N, LIF_brian(LIFENeuron(0.0, 0.0, 0.0, 1.0e-1, 50.0e-3, 0.0, 1.0, 1.0, 15.0e-3, 0.0e-3), 1.0, 1.0));
-	Population<LIF_brian> *pn5 = c.createPopulation(5, N, LIF_brian(LIFENeuron(0.0, 0.0, 0.0, 1.0e-1, 50.0e-3, 0.0, 1.0, 1.0, 15.0e-3, 0.0e-3), 1.0, 1.0));
+	auto pn0 = c.createPopulation(N, PoissonNeuron(10, 0));
+	//(real v_init, real v_rest, real v_reset, real cm, real tau_m, real tau_refrac, real tau_syn_E, real tau_syn_I, real v_thresh, real i_offset);
+	auto pn1 = c.createPopulation(N, LIF_brian(LIFENeuron(0.0, 0.0, 0.0, 0.9, 50.0e-3, 0.0, 1.0, 1.0, 0.5, 100.0e-1), 1, 1));
 
-	real * weight0 = NULL;
-	real * weight1 = NULL;
-	real * delay1 = NULL;
-	real * delay2 = NULL;
+	mt19937 gen;
+	gen.seed(1337);
+	uniform_real_distribution<> iif;
+	for (int src = 0; src < N; src++)
+		for (int dst = 0; dst < N; dst++)
+			if (iif(gen) < 0.05)
+				c.connect(0, src, 1, dst, 0.1, 0.01);
 
-	if (load) { 
-		printf("LOAD DATA...\n");
-		weight0 = loadArray("weight0.csv", N*N);
-		weight1 = loadArray("weight1.csv", N*N);
-		delay1 = loadArray("delay1.csv", N*N);
-		delay2 = loadArray("delay2.csv", N*N);
-		printf("LOAD DATA FINISHED\n");
-	} else {
-		printf("GENERATE DATA...\n");
-		//real * array = getConstArray(weight_value, num);
-		weight0 = (real*)malloc(sizeof(real) * N * N);
-		weight1 = (real*)malloc(sizeof(real) * N * N);
+	//c.connect(pn0, pn1, 0.01, 0.01, Excitatory);
 
-		for (int i=0; i<N*N; i++) {
-		    weight0[i] = 0.002 + (double)(i)/(double)(N*N) * 0.02;
-		    weight1[i] = 0.022 - (double)(i)/(double)(N*N) * 0.02;
-		}
+	SGSim sg(&c, 0.01);
+	sg.run(10);
 
-		//weight2 = getRandomArray((real)20e-3, N*N);
-		delay1 = getConstArray((real)1e-4, N*N);
-		delay2 = getConstArray((real)2e-4, N*N);
-		printf("GENERATE DATA FINISHED\n");
-	}
-
-	//Network.connect(population1, population2, weight_array, delay_array, Exec or Inhi array, num)
-	c.connect(pn0, pn1, weight0, delay1, NULL, N*N);
-	c.connect(pn1, pn2, weight0, delay1, NULL, N*N);
-	c.connect(pn2, pn3, weight0, delay1, NULL, N*N);
-	c.connect(pn3, pn4, weight1, delay1, NULL, N*N);
-	c.connect(pn4, pn5, weight1, delay1, NULL, N*N);
-	SGSim sg(&c, 1.0e-4);
-	sg.run(0.1);
-
-	if (!load) {
-		printf("SAVE DATA...\n");
-		saveArray("weight0.csv", weight0, N*N);
-		saveArray("weight1.csv", weight1, N*N);
-		saveArray("delay1.csv", delay1, N*N);
-		saveArray("delay2.csv", delay2, N*N);
-		printf("SAVE DATA FINISHED\n");
-	}
-	
 	return 0;
 } 

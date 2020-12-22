a7c85165805d906549f788edcbb56cfc6a2113f4
dkb <dennis@bautembach.de>
2020-12-22 17:34:40 +0200

    Implement synth. model

--- src/neuron/poisson/poisson.cu
+++ src/neuron/poisson/poisson.cu
@@ -7,7 +7,7 @@
 __global__ void update_poisson_neuron(GPoissonNeurons *d_neurons, int const num, int const start_id)
 {
 	for (int idx = blockIdx.x * blockDim.x + threadIdx.x; idx < num; idx += blockDim.x * gridDim.x) {
-		if (curand_uniform(&d_neurons->p_state[idx]) < 0.002f) {
+		if (curand_uniform(&d_neurons->p_state[idx]) < d_neurons->p_rate[idx]) {
 			gFireCount[start_id + idx]++;
 			gFiredTable[gFiredTableCap*gCurrentIdx + atomicAdd(&(gFiredTableSizes[gCurrentIdx]), 1)] = start_id + idx;
 		}
